a484fb515c766f62c3c19e651bf8d7967d0bdd0f
Carsten Bleicker <carsten@bleicker.de>
2015-04-21 14:45:45 +0200

    Introduce EntityManager

---
+++ src/EntityManager.php
@@ -0,0 +1,11 @@
+<?php
+namespace Bleicker\Persistence;
+
+use Doctrine\ORM\EntityManager as EntityManagerOrigin;
+
+/**
+ * Class EntityManager
+ */
+class EntityManager extends EntityManagerOrigin implements EntityManagerInterface {
+
+}
---
+++ src/EntityManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bleicker\Persistence;
+
+use Doctrine\ORM\EntityManagerInterface as EntityManagerInterfaceOrigin;
+
+/**
+ * Interface EntityManagerInterface
+ *
+ * @package Bleicker\Persistence
+ */
+interface EntityManagerInterface extends EntityManagerInterfaceOrigin {
+
+}
